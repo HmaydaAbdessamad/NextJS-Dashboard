83eca071c93f17214602225fbf4d43c5e38d3a1f
Hmayda <abdoessamadhmayda@gmail.com>
2024-09-16 03:01:11 +0100

    streaming +skeleton

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -23,10 +23,10 @@ export default async function Page() {
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        /> *
+        />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <RevenueChart revenue={revenue}  />
+                 <RevenueChart revenue={revenue}  />
                  <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
